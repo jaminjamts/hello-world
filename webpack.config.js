78a7a03e361d83c9b2099463e85a4fb80bfe0888
jaminjamts <jamka888@gmail.com>
2023-09-06 11:33:15 +0900

    large hero section, intro section finished

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+const postCSSPlugins = [
+  require("postcss-mixins"),
+  require("postcss-import"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer"),
+];
+
+module.exports = {
+  entry: "../platinum-travel-site/app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+
+  devServer: {
+    static: function(app, server){
+      server._watch("./app/**/*.html");
+    },
+    static: path.join(__dirname, "app"),
+    hot: true,
+    port: 3002,
+
+    host: "0.0.0.0"
+  },
+
+  mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: { postcssOptions: { plugins: postCSSPlugins } },
+          },
+        ],
+      },
+    ],
+  },
+};
